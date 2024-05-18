f15f404a03b6c718b239037aaf1cbd2150fec80d
Lady Nuñez <laadynz@hotmail.com>
2024-05-18 09:34:39 -0700

    Cambios

--- js/funciones.js
+++ js/funciones.js
@@ -2,5 +2,14 @@ function buscarProductos(event) {
     if(event.key == 'Enter') {
         var codigo = document.getElementById('codigo').value;
         alert('Buscando el producto con código: ' + codigo );
+        for(var i = 0; i < productos.length; i++) {
+            if(productos[i][0] == codigo) {
+               /* document.getElementById('nombre').value = productos[i][1];
+                document.getElementById('precio').value = productos[i][2];*/
+                alert('Producto encontrado: ' + productos[i][1] + ' ' + productos[i][2]);
+                break;
+            }
+        }
     }
 }
+
